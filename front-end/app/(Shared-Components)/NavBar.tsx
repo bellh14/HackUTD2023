105d99c8cf0b00a9c91cbf48c0cb0cef34766874
bellh14 <haybell965@gmail.com>
2023-11-04 14:29:06 -0500

    started navbar

---
+++ front-end/app/(Shared-Components)/NavBar.tsx
@@ -0,0 +1,64 @@
+"use client";
+import React from "react";
+import { Avatar, Dropdown, Navbar } from "flowbite-react";
+import Link from "next/link";
+import Image from "next/image";
+import exampleLogo from "../../public/next.svg"
+
+type Props = {};
+
+const NavBar = (props: Props) => {
+    return (
+        <Navbar fluid rounded>
+            <Navbar.Brand href="https://flowbite-react.com">
+                <Image
+                  src={exampleLogo}
+                  width={64}
+                  height={64}
+                  className={"mr-4"}
+                  alt={"Example Logo"}
+                />
+                <span className="self-center whitespace-nowrap text-xl font-semibold text-black dark:text-white">
+                    Name
+                </span>
+            </Navbar.Brand>
+            <div className="flex md:order-2">
+                <Dropdown
+                    arrowIcon={false}
+                    inline
+                    label={
+                        <Avatar
+                            alt="User settings"
+                            img="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+                            rounded
+                        />
+                    }
+                >
+                    <Dropdown.Header>
+                        <span className="block text-sm">Bonnie Green</span>
+                        <span className="block truncate text-sm font-medium">
+                            name@flowbite.com
+                        </span>
+                    </Dropdown.Header>
+                    <Dropdown.Item>Dashboard</Dropdown.Item>
+                    <Dropdown.Item>Settings</Dropdown.Item>
+                    <Dropdown.Item>Earnings</Dropdown.Item>
+                    <Dropdown.Divider />
+                    <Dropdown.Item>Sign out</Dropdown.Item>
+                </Dropdown>
+                <Navbar.Toggle />
+            </div>
+            <Navbar.Collapse>
+                <Navbar.Link href="#" active>
+                    Home
+                </Navbar.Link>
+                <Navbar.Link href="#">About</Navbar.Link>
+                <Navbar.Link href="#">Services</Navbar.Link>
+                <Navbar.Link href="#">Graphs</Navbar.Link>
+                <Navbar.Link href="#">Contact</Navbar.Link>
+            </Navbar.Collapse>
+        </Navbar>
+    );
+};
+
+export default NavBar;
