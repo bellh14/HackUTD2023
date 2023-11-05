b7aab367f54cb013ce3e3a0faed936ff3e3fec9e
meyli <meylicolmenero@gmail.com>
2023-11-05 00:24:53 -0500

    fixed home page

--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -7,8 +7,8 @@ import ApprovalSection from "./(home)/ApprovalSection";
 export default function Home() {
     return (
 
-        <main className="items-center justify-between p-24 bg-white h-screen">
-          <div className="h-400 sm:h-64 xl:h-80 2xl:h-96" style={{height:'850px'}}>
+        <main className="items-center justify-between pb-24  bg-white ">
+          <div className="h-400 sm:h-64 xl:h-80 2xl:h-96 pb-20" style={{height:'850px'}}>
             <Carousel slideInterval={5000} >
               <div>
                 <img src="https://images.unsplash.com/photo-1564156280315-1d42b4651629?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGZhbWlseSUyMHJlbm92YXRpbmd8ZW58MHx8MHx8fDA%3D" alt="..." style={{ width: '100%', height:'fill', objectFit:'contain'}} />
@@ -27,7 +27,7 @@ export default function Home() {
               </div>
             </Carousel>
           </div>
-          <ApprovalSection approvalStatus="Y" loanAmount={100000} monthlyPayment={2500}/>  
+          <ApprovalSection  approvalStatus="Y" loanAmount={100000} monthlyPayment={2500}/>  
         </main>
     );
 }
